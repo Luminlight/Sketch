f791a06b4e1b6290b96fe595d7344f8fca12bdb5
Luminlight <razznikx@gmail.com>
2022-06-20 18:36:32 -0400

    Updated comments to describe functionality. Made slight modifications to colors. Removed functions to test functionality of coloring divs.

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-// Create a 16x16 grid of square divs
-// Create divs in JS
+const gridOne = document.querySelector("[data-grid-container]");
+const gridTwo = document.querySelectorAll("[data-grid-item]");
 
-// Total number of divs
+// Displayed number of squares in grid (use squared number)
 let totalDivs = 16;
 // The column & row amount
 let totalDivRoot = Math.sqrt(totalDivs);
 
 // Load div on page load
-document.body.onload = containerDiv;
+// document.body.onload = containerDiv;
 
 // Create container div
-function containerDiv() {
-  const divForContainer = document.createElement("div");
-  // Add class, method 1
-  divForContainer.className = "container";
-  divForContainer.setAttribute(
-    "style",
-    `grid-template-columns: repeat(${totalDivRoot}, 1fr);`
-  );
-  document.body.append(divForContainer);
+const divForContainer = document.createElement("div");
+divForContainer.classList.add("container");
+divForContainer.dataset.gridContainer = "container";
+divForContainer.setAttribute(
+  "style",
+  `grid-template-columns: repeat(${totalDivRoot}, 1fr);`
+);
+document.body.append(divForContainer);
 
-  addGridDiv();
+// Append divs to container div
+for (let i = 0; i < totalDivs; i++) {
+  const gridDiv = document.createElement("div");
+  gridDiv.classList.add("grid-item");
+  gridDiv.dataset.gridItem = "grid-item";
+  document.querySelector(".container").append(gridDiv);
 }
 
-// Create # of grid square divs
-function addGridDiv() {
-  // Append divs to container div
-  for (let i = 0; i < totalDivs; i++) {
-    const gridDiv = document.createElement("div");
-    // Add class, method 2
-    gridDiv.classList.add("grid-item");
-    document.querySelector(".container").append(gridDiv);
-  }
-}
+console.log(gridOne);
+console.log(gridTwo);
