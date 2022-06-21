13818a41263509026c1ebe8087cad628f03eef5e
Luminlight <razznikx@gmail.com>
2022-06-21 12:53:36 -0400

    Functions are back, now have the ability to color in divs with black on hover.

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-const gridOne = document.querySelector("[data-grid-container]");
-const gridTwo = document.querySelectorAll("[data-grid-item]");
+// Load div on page load
+document.body.onload = containerDiv;
 
 // Displayed number of squares in grid (use squared number)
-let totalDivs = 16;
+let totalDivs = 1600;
 // The column & row amount
 let totalDivRoot = Math.sqrt(totalDivs);
 
-// Load div on page load
-// document.body.onload = containerDiv;
-
 // Create container div
-const divForContainer = document.createElement("div");
-divForContainer.classList.add("container");
-divForContainer.dataset.gridContainer = "container";
-divForContainer.setAttribute(
-  "style",
-  `grid-template-columns: repeat(${totalDivRoot}, 1fr);`
-);
-document.body.append(divForContainer);
-
-// Append divs to container div
-for (let i = 0; i < totalDivs; i++) {
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("grid-item");
-  gridDiv.dataset.gridItem = "grid-item";
-  document.querySelector(".container").append(gridDiv);
+function containerDiv() {
+  const divForContainer = document.createElement("div");
+  divForContainer.classList.add("container");
+  //divForContainer.dataset.gridContainer = "container";
+  divForContainer.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${totalDivRoot}, 1fr);`
+  );
+  document.body.append(divForContainer);
+  addGridDiv();
+  colorDiv();
 }
 
-console.log(gridOne);
-console.log(gridTwo);
+// Append divs to container div
+function addGridDiv() {
+  for (let i = 0; i < totalDivs; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid-item");
+    //gridDiv.dataset.gridItem = "grid-item";
+    document.querySelector(".container").append(gridDiv);
+  }
+}
+
+function colorDiv() {
+  // Very basic hover effect
+  const gridOne = document.querySelectorAll(".grid-item");
+
+  for (let i = 0; i < gridOne.length; i++) {
+    gridOne[i].addEventListener("mouseover", () => {
+      gridOne[i].style.backgroundColor = "black";
+    });
+  }
+}
